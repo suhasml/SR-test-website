a129e82be20ceb515397c667c551c26c8565b1c3
Suhas M L <suhasml37@gmail.com>
2024-02-04 19:14:21 +0530

    added session replay tracker

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,58 @@
-import Feature from "../components/Feature";
-import Pricing from "../components/Pricing";
-import Hero from "../components/Hero";
-import Layout from "../components/Layout/Layout";
-import SeoHead from "../components/SeoHead";
+// import Feature from "../components/Feature";
+// import Pricing from "../components/Pricing";
+// import Hero from "../components/Hero";
+// import Layout from "../components/Layout/Layout";
+// import SeoHead from "../components/SeoHead";
+// import Tracker from '@openreplay/tracker';
+// import { useEffect } from "react";
 
-export default function Home() {
+// const tracker = new Tracker({
+//   projectKey: "L5N69rX6SwdTrr8rvpNX",  
+// });
+
+
+// export default function Home() {
+
+//   useEffect(() => {
+//     tracker.start();
+//   }, []);
+
+//   return (
+//     <>
+//       <SeoHead title='LaslesVPN Landing Page' />
+//       <Layout>
+//         <Hero />
+//         <Feature />
+//         <Pricing />
+//       </Layout>
+//     </>
+//   );
+// }
+
+import React, { useEffect } from 'react';
+import Feature from '../components/Feature';
+import Pricing from '../components/Pricing';
+import Hero from '../components/Hero';
+import Layout from '../components/Layout/Layout';
+import SeoHead from '../components/SeoHead';
+
+// Import OpenReplay tracker conditionally
+let Tracker;
+if (typeof window !== 'undefined') {
+  Tracker = require('@openreplay/tracker').default;
+}
+
+const Home = () => {
+  useEffect(() => {
+    // Check if we are in the browser environment before initializing OpenReplay
+    if (Tracker) {
+      const tracker = new Tracker({
+        projectKey: 'L5N69rX6SwdTrr8rvpNX',  // Replace with your OpenReplay project key
+      });
+      tracker.start();
+    }
+  }, []); // Run this effect only once on component mount
+
   return (
     <>
       <SeoHead title='LaslesVPN Landing Page' />
@@ -16,3 +64,5 @@ export default function Home() {
     </>
   );
 }
+
+export default Home;
